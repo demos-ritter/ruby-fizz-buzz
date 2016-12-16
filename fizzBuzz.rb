53681972696f93a06cba30420f01338d5be00405
bmeritter <bme_ritter@foxmail.com>
2016-12-16 09:28:10 +0800

    update fizzBuzz.rb

--- fizzBuzz.rb
+++ fizzBuzz.rb
@@ -1,20 +1,14 @@
 def fizz_buzz(n)
   result = []
   for i in 0...n
-    if ((i+1) % 3 == 0)
-      if ((i+1) % 5 == 0)
-        result[i] = 'FizzBuzz'
-      else
-        result[i] = 'Fizz'
-
-      end
-
+    if ((i+1) % 15 == 0) then
+      result[i] = 'FizzBuzz'
+    elsif ((i+1) % 3 == 0)
+      result[i] = 'Fizz'
+    elsif ((i + 1) % 5 == 0) then
+      result[i] = 'Buzz'
     else
-      if ((i+1) % 5 == 0)
-        result[i] ='Buzz'
-      else
-        result[i] =(i + 1).to_s
-      end
+      result[i] = (i+1).to_s
     end
   end
   return result
